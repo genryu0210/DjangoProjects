175ad4b5d9b70b344f3cacf38fdfd5fa8df34c8c
genryu0210 <kenta0210ken@yahoo.co.jp>
2022-08-23 17:30:35 +0900

    change

--- todoproject/todo/urls.py
+++ todoproject/todo/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import TodoList, TodoDetail
+from .views import TodoList, TodoDetail, TodoCreate, TodoDelete, TodoUpdate
+
 urlpatterns = [
-    path('list/', TodoList.as_view()),
-    path('detail/<pk>', TodoDetail.as_view())
+    path('list/', TodoList.as_view(), name='list'),
+    path('detail/<pk>', TodoDetail.as_view(), name='detail'),
+    path('create/', TodoCreate.as_view(), name='create'),
+    path('delete/<pk>', TodoDelete.as_view(), name='delete'),
+    path('update/<pk>', TodoUpdate.as_view(), name='update')
 ]
